ef45aaf774e6237be9c5bffd47a1ff6a6f033c54
OKontey <157806715+OKontey@users.noreply.github.com>
2024-01-30 00:14:19 +0700

    Update fluid_frame_ad.js
    improved readability of variables

--- js/fluid_frame_ad.js
+++ js/fluid_frame_ad.js
@@ -15,11 +15,11 @@ function makeFluid(){
 			let frameWidth = $(iframes[i]).outerWidth(),
 				frameHeight = $(iframes[i]).outerHeight(),
 				frameParent = $(iframes[i]).closest("div"),
-				parent_w = frameParent.outerWidth();
+				parentWidth = frameParent.outerWidth();
 
-			if(parent_w < frameWidth){
-				var per_not_100F = Number(calcX(frameWidth,parent_w,));
-				numScale = per_not_100F/100;
+			if(parentWidth < frameWidth){
+				var perNot100 = Number(calcX(frameWidth,parentWidth,));
+				numScale = perNot100/100;
 			}else{
 				numScale = 1;
 			}
@@ -39,9 +39,9 @@ function makeFluid(){
 		$(el).css({'overflow': 'hidden','height': ''+Number(elHeight*100)/100 * Number(scale)+''});
 	}
 	
-	function calcX(width,width_el){
-		r =  width/100, p =  width_el/r, final_prc = parseInt(p*100)/100;
-		return final_prc;
+	function calcX(frame,parentE){
+		r =  frame/100, p =  parentE/r, finalPercent = parseInt(p*100)/100;
+		return finalPercent;
 	}
 	function stringifyr(val){
 		var scl = JSON.stringify(val);
